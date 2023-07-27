6b1751edeccf934792dda4f0d5103c532fb8e44a
alichatraei <avida.chatraee2001@yahoo.com>
2023-07-27 12:17:06 +0330

    feature: check-if-has-account

--- context/NFTContext.tsx
+++ context/NFTContext.tsx
@@ -2,18 +2,50 @@ import { useState, useEffect, createContext, ReactNode } from "react";
 import { NFTMarketAddressAbi, MarketAddress } from "./constants";
 
 export interface INFTContext {
-  nft:string
+  nft:string;
+  currentAccounts:any[];
+  connectWallet:()=>void
 }
 
 const initialNFTContext = {
-  nft:""
+  nft:"",
+  currentAccounts:[],
+  connectWallet:()=>{}
 }
 
 export const NFTContext = createContext<INFTContext>(initialNFTContext);
 
 export const NFTContextProvider = ({ children }:{children:ReactNode}) => {
+  const [currentAccounts,setCurrentAccounts] = useState([])
   const nft = "ETH";
-  return <NFTContext.Provider value={{ nft }}>
+
+  const checkIfWalletIsConnected=async()=>{
+    if(!(window as any).ethereum) return alert("Please install MetaMask")
+
+    const accounts = await (window as any).ethereum.request({method: "eth_accounts"})
+
+    if(accounts.length){
+      setCurrentAccounts(accounts)
+      console.log(currentAccounts);
+    } else console.log("No accounts found.")
+    
+  }
+
+  const connectWallet = async ()=>{
+    if(!(window as any).ethereum) return alert("Please Install MetaMask")
+
+    const account = (window as any).ethereum.request({method:"eth_requestAccounts"});
+
+    setCurrentAccounts(account[0])
+
+    window.location.reload()
+  }
+
+  useEffect(()=>{ 
+    checkIfWalletIsConnected()
+  },[])
+
+  return <NFTContext.Provider value={{ nft ,connectWallet, currentAccounts}}>
     {children}
     </NFTContext.Provider>;
 };
